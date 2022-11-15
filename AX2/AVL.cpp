d8cb8e87d473db8f457770144dcaa0306cb2b129
C-Shep <2101007@uad.ac.uk>
2022-11-15 18:10:14 +0000

    i am a god in human clothing lightning bolts shoot fro my fingertips
    AHAHAHAHHAHAHAHAHHAHA

--- AX2/AVL.cpp
+++ AX2/AVL.cpp
@@ -51,7 +51,7 @@ AVL* AVL::insertNode(AVL* r, int key)
 {
 	if (r == nullptr)
 	{
-		r = new AVL(key);
+		r = new AVL(key);//if theres nothing here, make something here
 		return r;
 	}
 
@@ -70,7 +70,6 @@ AVL* AVL::insertNode(AVL* r, int key)
 	else {
 		return r; //this is the coconut
 	}
-
 	
 	r->height = 1 + max(getHeight(r->left), getHeight(r->right));
 
@@ -102,8 +101,174 @@ AVL* AVL::insertNode(AVL* r, int key)
 	return r;
 }
 
+//AVL* AVL::deleteNode(AVL* r, int key)
+//{
+//	if (r == nullptr)
+//	{
+//		return r;//works like a dream :))
+//	}
+//
+//	if (r->data == key)
+//	{
+//		//r->data = NULL;
+//		r = nullptr; // no dupes
+//		return r;
+//	}
+//
+//	if (r->data < key)
+//	{
+//		r->right = deleteNode(r->right, key);
+//	}
+//	else if (r->data > key) {
+//		r->left = deleteNode(r->left, key);
+//	}
+//	else {
+//		r = nullptr; // is a coconut a fruit? i guess its a nut right? but how many nuts have milk in them? is it really milk or is it like just a funny coloured water? god this is so stupid
+//		return r; //this is the coconut
+//	}
+//
+//	r->height = 1 + max(getHeight(r->left), getHeight(r->right));
+//
+//	int balance = getBalance(r);
+//
+//	// Left Left Case
+//	if (balance > 1 && key < r->left->data)
+//		return rightRotate(r);
+//
+//	// Right Right Case
+//	if (balance < -1 && key > r->right->data)
+//		return leftRotate(r);
+//
+//	// Left Right Case
+//	if (balance > 1 && key > r->left->data)
+//	{
+//		r->left = leftRotate(r->left);
+//		return rightRotate(r);
+//	}
+//
+//	// Right Left Case
+//	if (balance < -1 && key < r->right->data)
+//	{
+//		r->right = rightRotate(r->right);
+//		return leftRotate(r);
+//	}
+//
+//	/* return the (unchanged) node pointer */
+//	return r;
+//}
+
 AVL* AVL::deleteNode(AVL* r, int key)
 {
+	AVL* temp;
+
+	if (r == nullptr)
+	{
+		return r;
+	}
+	else
+	{
+		if (key > r->data)
+		{
+			r->right = deleteNode(r->right, key);
+			int balance = getBalance(r);
+
+			// Left Left Case
+			if (balance > 1 && key < r->left->data)
+				return rightRotate(r);
+
+			// Right Right Case
+			if (balance < -1 && key > r->right->data)
+				return leftRotate(r);
+
+			// Left Right Case
+			if (balance > 1 && key > r->left->data)
+			{
+				r->left = leftRotate(r->left);
+				return rightRotate(r);
+			}
+
+			// Right Left Case
+			if (balance < -1 && key < r->right->data)
+			{
+				r->right = rightRotate(r->right);
+				return leftRotate(r);
+			}
+		}
+		else
+		{
+			if (key < r->data)
+			{
+				r->left = deleteNode(r->left, key);
+				int balance = getBalance(r);
+
+				// Left Left Case
+				if (balance > 1 && key < r->left->data)
+					return rightRotate(r);
+
+				// Right Right Case
+				if (balance < -1 && key > r->right->data)
+					return leftRotate(r);
+
+				// Left Right Case
+				if (balance > 1 && key > r->left->data)
+				{
+					r->left = leftRotate(r->left);
+					return rightRotate(r);
+				}
+
+				// Right Left Case
+				if (balance < -1 && key < r->right->data)
+				{
+					r->right = rightRotate(r->right);
+					return leftRotate(r);
+				}
+			}
+			else
+			{
+				if (r->right != nullptr)
+				{
+					temp = r->right;
+
+					while (temp->left != nullptr)
+					{
+						temp = temp->left;
+					}
+
+					r->data = temp->data;
+					r->right = deleteNode(r->right, temp->data);
+
+					int balance = getBalance(r);
+
+					// Left Left Case
+					if (balance > 1 && key < r->left->data)
+						return rightRotate(r);
+
+					// Right Right Case
+					if (balance < -1 && key > r->right->data)
+						return leftRotate(r);
+
+					// Left Right Case
+					if (balance > 1 && key > r->left->data)
+					{
+						r->left = leftRotate(r->left);
+						return rightRotate(r);
+					}
+
+					// Right Left Case
+					if (balance < -1 && key < r->right->data)
+					{
+						r->right = rightRotate(r->right);
+						return leftRotate(r);
+					}
+				}
+				else
+				{
+					return(r->left);
+				}
+			}
+		}
+	}
+	//T->ht = height(T);
 	return r;
 }
 
@@ -114,9 +279,6 @@ AVL* AVL::leftRotate(AVL* r)
 
 	newRoot->left = r;
 	r->right = subtreeToTransplant;
-
-	//update height of root (1+max...)
-	//update height of NR (1+max...)
 
 	// Update heights
 	r->height = max(getHeight(r->left), getHeight(r->right) + 1);
@@ -136,9 +298,7 @@ AVL* AVL::rightRotate(AVL* r)
 	newRoot->right = r;
 	r->left = subtreeToTransplant;
 
-	//update height of root (1+max...)
-	//update height of NR (1+max...)
-	// 
+
 	// Update heights
 	r->height = max(getHeight(r->left), getHeight(r->right) + 1);
 
@@ -165,11 +325,11 @@ int main()
 {
 	AVL* tree = new AVL(3);
 
-	for (int i = 1; i <= 100; ++i)
+	for (int i = 1; i <= 10; ++i)
 	{
 		tree = tree->insertNode(tree, i);
 	}
-	tree = tree->insertNode(tree, );
+	tree = tree->deleteNode(tree, 4);
 	tree->inOrder(tree);
 
 	std::cout << tree->data;
